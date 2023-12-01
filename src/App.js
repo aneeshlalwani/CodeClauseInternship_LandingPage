81d55736bc9494a8b05cc147bcfc640fc590cf81
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2023-12-02 01:17:03 +0500

    added about component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // importing components
 import HeroSection from './components/HeroSection';
 import Exploration from './components/Exploration';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <HeroSection />
+      <About />
       <Exploration />
     </div>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import food from "../assests/images/traditional_dishes.png"
+const About = () => {
+    const aboutStyle = {
+        background: "#000"
+    }
+    const ImageSectionStyle = {
+        
+        background: "orange",
+        borderRadius: "10px",
+        width: "80%",
+    }
+    const foodImage = {
+        width: "50%",
+        height: "auto",
+        objectFit: "cover",
+    }
+  return (
+    <>
+      <div style={aboutStyle}>
+        <h2 className="text-center text-white pt-4">
+          Explore the Pakistan's traditional and authentic food
+        </h2>
+        <div className="py-3 text-center mx-auto" style={ImageSectionStyle}>
+        <p className="px-5">
+          Discover the delicious world of Pakistan's authentic cuisine! Immerse
+          yourself in the flavors of tradition, with every bite telling a story
+          of aromatic spices, bold textures, and timeless recipes. Let the heart
+          of Pakistani cuisine take you on a flavorful journey, inviting you to
+          savor the rich culinary heritage of this vibrant nation.
+        </p>
+        <img src={food} alt="traditional-dishes-of-Pakistan" className="" style={foodImage}/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
